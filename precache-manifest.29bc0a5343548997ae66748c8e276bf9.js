bc84da6a71c9be9ea2da9c8ef2047d0e4bc7e0f4
GeovanniG <32276348+GeovanniG@users.noreply.github.com>
2019-09-23 08:31:29 -0700

    Updates

---
+++ precache-manifest.29bc0a5343548997ae66748c8e276bf9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7db128558b124a1d3775abbcfcdd55",
+    "url": "/Portfolio-v2/index.html"
+  },
+  {
+    "revision": "176f30905ace07fa518b",
+    "url": "/Portfolio-v2/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "13c5bd13c54fe2338716",
+    "url": "/Portfolio-v2/static/css/3.1a3091b4.chunk.css"
+  },
+  {
+    "revision": "5bbb16659543b1399588",
+    "url": "/Portfolio-v2/static/css/main.03b29aa5.chunk.css"
+  },
+  {
+    "revision": "176f30905ace07fa518b",
+    "url": "/Portfolio-v2/static/js/2.a5996163.chunk.js"
+  },
+  {
+    "revision": "13c5bd13c54fe2338716",
+    "url": "/Portfolio-v2/static/js/3.325847cf.chunk.js"
+  },
+  {
+    "revision": "b72deca99ad8daedc51f",
+    "url": "/Portfolio-v2/static/js/4.0ec9ce9c.chunk.js"
+  },
+  {
+    "revision": "2cc17831ec701d437d33",
+    "url": "/Portfolio-v2/static/js/5.b6e2bc30.chunk.js"
+  },
+  {
+    "revision": "5bbb16659543b1399588",
+    "url": "/Portfolio-v2/static/js/main.ebfde4bb.chunk.js"
+  },
+  {
+    "revision": "0f7b07ef51999fe4d4e7",
+    "url": "/Portfolio-v2/static/js/runtime~main.bfa2ba09.js"
+  },
+  {
+    "revision": "46b114b9e9b630bae11a5b37af06ab6c",
+    "url": "/Portfolio-v2/static/media/bg9.46b114b9.svg"
+  }
+]);
---
+++ static/js/5.b6e2bc30.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpclient=window.webpackJsonpclient||[]).push([[5],{42:function(n,o,t){"use strict";t.r(o);var e=t(1),i=t.n(e);o.default=function(){return i.a.createElement("p",null,"this is from Works To do: Scroll animation")}}}]);
+//# sourceMappingURL=5.b6e2bc30.chunk.js.map
